0b291d44a0824929cf7d2001c3b7284e1c47eb81
PascualVera <pascualvera.994@gmail.com>
2022-09-01 01:17:25 +0200

    patterns added

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,14 +2,14 @@ import "./Main.css";
 import Header from "../components/Header/Header";
 import Navbar from "../components/Navbar/Navbar";
 import Conway from "../components/Conway/Conway";
-import { pulsar } from "../utiils";
+
 export default function Main() {
 	return (
 		<main className="container">
-			{/* <article className="header_wrapper">
+			<article className="header_wrapper">
 				<Navbar />
 				<Header />
-			</article> */}
+			</article>
 			<Conway />
 		</main>
 	);
