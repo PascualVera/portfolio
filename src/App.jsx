fff44f5451d673f98886d61bb3e18976550359f6
Vera Pascual <pascual.vera@solera.com>
2023-10-25 13:42:47 +0200

    header component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
 import style from './App.module.css';
-import meVectoriced from './assets/meVectoricedblue2.svg';
-import Conway from './components/Conway/Conway';
-import { pulsarSchema as schema } from './utils/schema';
-import title from './assets/title.svg';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className={style.App}>
-      <div className={style.section_wrapper}>
-        <div className={style.conway_container}>
-          <Conway schema={schema} runningMode="conway" />
-        </div>
-        <div className={style.title}>
-          <h1 title="Pascual Vera">
-            <img src={title} alt="title" />
-            <h2>Software developer</h2>
-          </h1>
-        </div>
-
-        <div className={style.backdrop} />
-        <picture className={style.img_container}>
-          <img src={meVectoriced} alt="me posing for inkscape" />
-        </picture>
-      </div>
+      <Header />
     </div>
 
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import style from './Header.module.css';
+import meVectoriced from '../../assets/meVectoriced.svg';
+import Conway from '../Conway/Conway';
+import { pulsarSchema as schema } from '../../utils/schema';
+import title from '../../assets/title.svg';
+
+export default function Header() {
+  return (
+    <header className={style.header}>
+      <div className={style.conway_container}>
+        <Conway schema={schema} runningMode="conway" />
+      </div>
+      <div className={style.title}>
+        <h1 title="Pascual Vera">
+          <img src={title} alt="title" />
+          <h2>Software developer</h2>
+        </h1>
+      </div>
+
+      <div className={style.backdrop} />
+      <picture className={style.img_container}>
+        <img src={meVectoriced} alt="me posing for inkscape" />
+      </picture>
+    </header>
+  );
+}
