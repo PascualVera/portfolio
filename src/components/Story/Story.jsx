2275ed34eca58bf315c039b8c1ebef583d71fa43
Vera Pascual <pascual.vera@solera.com>
2023-10-26 16:42:18 +0200

    add box for projects

--- src/components/Story/Story.jsx
+++ src/components/Story/Story.jsx
@@ -3,8 +3,11 @@ import style from './Story.module.css';
 export default function Story() {
   return (
     <section className={style.container}>
-      <div className={style.title_wrapper}>
-        <h2 className={style.title}>Projects</h2>
+      <div className={style.projects_content}>
+        <div className={style.title_wrapper}>
+          <h2 className={style.title}>Company Projects</h2>
+          <h2 className={style.title}>Freelance</h2>
+        </div>
       </div>
 
     </section>
