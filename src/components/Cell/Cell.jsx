0c2020bc77a1fc9a3aee79b59c6a4132d8ef6f9d
Vera Pascual <pascualveradev@gmail.com>
2023-10-26 17:22:37 +0200

    add cell controller

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -1,13 +1,23 @@
 import style from './Cell.module.css';
 
-export default function Cell({ state, color }) {
+export default function Cell({ state, color, content }) {
   const cellColor = {
     backgroundColor: `${color}`,
+  };
+  const cellFace = {
+    position: 'absolute',
+    width: '2vw',
+    height: '2vw',
+    transition: 'all 0.3s ease',
   };
 
   return (
     <div id="cell" className={`${state ? style.cell_alive : style.cell}`}>
-      <div style={cellColor} className={`${style.cell_face} ${style.cell_face_front}  ${state ? style.face_alive : null}`} />
+      <div style={cellColor} className={`${cellFace} ${style.cell_face_front}  ${state ? style.face_alive : null}`}>
+        {' '}
+        {content}
+        {' '}
+      </div>
       <div style={cellColor} className={`${style.cell_face} ${style.cell_face_right}   ${state ? style.face_alive : null}`} />
       <div style={cellColor} className={`${style.cell_face} ${style.cell_face_left}   ${state ? style.face_alive : null}`} />
       <div style={cellColor} className={`${style.cell_face} ${style.cell_face_top}   ${state ? style.face_alive : null}`} />
--- src/components/Story/Story.jsx
+++ src/components/Story/Story.jsx
@@ -1,12 +1,19 @@
 import style from './Story.module.css';
+import Cell from '../Cell/Cell';
 
 export default function Story() {
   return (
     <section className={style.container}>
       <div className={style.projects_content}>
         <div className={style.title_wrapper}>
-          <h2 className={style.title}>Company Projects</h2>
-          <h2 className={style.title}>Freelance</h2>
+          <Cell content="P" state />
+          <Cell content="R" state />
+          <Cell content="O" state />
+          <Cell content="J" state />
+          <Cell content="E" state />
+          <Cell content="C" state />
+          <Cell content="T" state />
+          <Cell content="S" state />
         </div>
       </div>
 
